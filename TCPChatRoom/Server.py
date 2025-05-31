650ea971b755528fbaa1bc910b413e32c629f7c3
Gurumanie Singh <gurumanie.singh@gmail.com>
2025-05-31 18:12:56 -0500

    Minor formatting changes for output

--- TCPChatRoom/Server.py
+++ TCPChatRoom/Server.py
@@ -40,7 +40,7 @@ def receive():
         aliases.append(alias)
         clients.append(client)
         print(f"The alias of this client is {alias}".encode("utf-8"))
-        broadcast(f"{alias} has connected to the chat!".encode("utf-8"))
+        broadcast(f"{alias} has connected to the chat!\n".encode("utf-8"))
         client.send("You are now connected!".encode("utf-8"))
         thread = threading.Thread(target = handle_client, args = (client,))
         thread.start()
